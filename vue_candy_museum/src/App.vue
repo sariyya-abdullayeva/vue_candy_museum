2e33bbad806a435604a45900d2e0fdc70f341a79
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-12-18 12:10:06 +0200

    Added background color and navbar pill

--- vue_candy_museum/src/App.vue
+++ vue_candy_museum/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld/>
   </div>
 </template>
 
@@ -22,7 +22,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
